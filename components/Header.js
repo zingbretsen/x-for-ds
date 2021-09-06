51c0a66222fb8b23aff4a5e61f21446cbbffb5ff
Zach Ingbretsen <zach@zingbretsen.com>
2021-09-06 06:08:53 -0400

    Removes css from Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import '../styles/header.css'
 
 export default function Header() {
   return (
@@ -13,30 +14,6 @@ export default function Header() {
           </Link>
         </nav>
       </header>
-      <style jsx>{`
-        header {
-          width: 100%;
-          height: 100px;
-          border-bottom: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        nav {
-          width: calc(100% - 40px);
-          max-width: 1200px;
-          font-weight: bold;
-          font-size: 1.3rem;
-        }
-        nav a {
-          margin-right: 20px;
-          color: #00a395;
-          text-decoration: none;
-        }
-        nav a:hover {
-          text-decoration: underline;
-        }
-      `}</style>
     </>
   )
 }
