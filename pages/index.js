c9eb800d3a6082ed7c67244b22f5efa24e1742b1
Zach Ingbretsen <zach@zingbretsen.com>
2021-09-06 06:42:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Layout from '@components/Layout'
 import PostList from '@components/PostList'
 
-import './styles/globals.css'
+import '@styles/globals.css'
 
 import getPosts from '@utils/getPosts'
 
